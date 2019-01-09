0954684a90fe7d9bd9e4aae51e033b806fb2f067
chenzewu <46393368+chenzewu@users.noreply.github.com>
2019-01-09 17:01:06 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,12 @@ window.onresize = function(){
 window.onload = function() {
 	console.log("谢谢你这么好看还来看我的网站");
 	
+	if(document.body.clientWidth <= "1200"){
+            container.style.width = "1200px";
+        }else{
+    	    container.style.width = "100%";
+        }
+	
 	//主题切换导航部分js
 	var topCenterNav = document.getElementsByClassName("top-center-nav")[0];
 	var navLi = topCenterNav.getElementsByTagName("li");
